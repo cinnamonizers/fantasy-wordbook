ee4352f71adec80ed2507a948e39c6a523906734
Nicholas Paro <nichparo@amazon.com>
2019-07-25 14:51:25 -0700

    hover fix

--- src/components/main/functions/quotes.js
+++ src/components/main/functions/quotes.js
@@ -6,7 +6,7 @@ export default function quotes(quote, sanskrit, translit) {
   return (
     <React.Fragment>
       <div className="quote-item">
-        <p>{spanner(quote)}</p>
+        <p className="hoverGrab">{spanner(quote)}</p>
         <p className='hide sansk'>{sanskrit}</p>
         <p className='hide translit'>{translit}</p>
       </div>
