a6441d93d1fe6ce78012127fa5e7532cc6c44a0a
Nicholas Paro <nichparo@amazon.com>
2019-07-24 21:10:43 -0700

    added null check for word route

--- src/components/main/components/words.js
+++ src/components/main/components/words.js
@@ -5,39 +5,47 @@ import wordObjSlicer from '../functions/word-obj-slicer.js';
 import listBuilder from '../functions/definitions-builder.js';
 
 export default class WordsPage extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {}
   }
-  
+
   setter = () => {
     let wordsArr = getLocalStorage('wordObj');
-    return (
-      wordsArr.map((wordStored, idx) => {
-        let def = [];
-        let syn = [];
-        let exmp = [];
-
-        def.push(wordStored.definitions);
-        syn.push(wordStored.synonyms);
-        exmp.push(wordStored.examples);
-
-        def = wordObjSlicer(def[0]);
-        syn = wordObjSlicer(syn[0]);
-        exmp = wordObjSlicer(exmp[0]);
-
-        return (
-          <div className='definitionBox' key={idx}>
-            <li>Word Chosen: {wordStored.word}</li>
-            <li>******************</li>
-            <li>Quote: "{wordStored.quote}"</li>
-            {listBuilder('Definitions:', def)}
-            {listBuilder('Synonyms:', syn)}
-            {listBuilder('Examples:', exmp)}
-          </div>
-        )
-      })
-    ) 
+    if(wordsArr === null){
+      return (
+        <h1 className="defSynExStyle">Please select some words and come back later!</h1>
+      )
+    } else {
+      return (
+        wordsArr.map((wordStored, idx) => {
+          let def = [];
+          let syn = [];
+          let exmp = [];
+  
+          def.push(wordStored.definitions);
+          syn.push(wordStored.synonyms);
+          exmp.push(wordStored.examples);
+  
+          def = wordObjSlicer(def[0]);
+          syn = wordObjSlicer(syn[0]);
+          exmp = wordObjSlicer(exmp[0]);
+  
+          return (
+            <div className='wordBox'>
+              <div key={idx}>
+                <li className="defSynExStyle">Word Chosen: {wordStored.word}</li>
+                <li>******************</li>
+                <li>Quote: "{wordStored.quote}"</li>
+                {listBuilder('Definitions:', def)}
+                {listBuilder('Synonyms:', syn)}
+                {listBuilder('Examples:', exmp)}
+              </div>
+            </div>
+          )
+        })
+      )
+    } 
   }
 
   render() {
