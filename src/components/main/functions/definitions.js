4b8f86b75752e6b9367c0f0625a5dc2162bbaf94
Nicholas Paro <nichparo@amazon.com>
2019-07-25 21:26:30 -0700

    final updates and readme

--- src/components/main/functions/definitions.js
+++ src/components/main/functions/definitions.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import listBuilder from './definitions-builder.js';
 import wordObjSlicer from './word-obj-slicer.js';
+import wordObjBuilder from './word-obj-builder.js';
 
 export default function definitions(wordChosen, currentQuote, definition) {
-  const quote = [];
-  let def = [];
-  let syn = [];
-  let exmp = [];
+  let quote = [], def = [], syn = [], exmp = [];
 
   quote.push(currentQuote);
   def.push(definition[1]);
@@ -17,41 +13,38 @@ export default function definitions(wordChosen, currentQuote, definition) {
   let sanskritSeen = quote[0].children[0].children[1].innerText;
   let translitSeen = quote[0].children[0].children[2].innerText;
 
+  console.log(translitSeen);
+
   def = wordObjSlicer(def[0]);
   syn = wordObjSlicer(syn[0]);
   exmp = wordObjSlicer(exmp[0]);
+
   if(sanskritSeen === ''){
+    let classSet = 'defSynExStyle hide';
     return (
-      <React.Fragment>
-        <div className='definitionsBox'>
-          <ul className='categoryStyle'>
-            <li className='defSynExStyle'>Word Chosen: {wordChosen}</li>
-            <li>******************</li>
-            <li className='defSynExStyle'>Quote: "{quote[0].innerText}"</li>
-            {listBuilder('Definitions:', def)}
-            {listBuilder('Synonyms:', syn)}
-            {listBuilder('Examples:', exmp)}
-          </ul>
-        </div>
-      </React.Fragment>
+      wordObjBuilder(
+        wordChosen,
+        quote[0].innerText,
+        def,
+        syn,
+        exmp,
+        classSet
+      )
     );
   } else {
+    let classSet = 'defSynExStyle';
     return (
-      <React.Fragment>
-        <div className='definitionsBox'>
-          <ul className='categoryStyle'>
-            <li className='defSynExStyle'>Word Chosen: {wordChosen}</li>
-            <li>******************</li>
-            <li className='defSynExStyle'>Quote: "{quoteSeen}"</li>
-            <li className='defSynExStyle'>Sanskrit: "{sanskritSeen}"</li>
-            <li className='defSynExStyle'>Transliteration: "{translitSeen}"</li>
-            {listBuilder('Definitions:', def)}
-            {listBuilder('Synonyms:', syn)}
-            {listBuilder('Examples:', exmp)}
-          </ul>
-        </div>
-      </React.Fragment>
+      wordObjBuilder(
+        wordChosen,
+        quoteSeen,
+        def,
+        syn,
+        exmp,
+        classSet,
+        null,
+        sanskritSeen,
+        translitSeen
+      )
     );
-  }
-  
+  } 
 }
